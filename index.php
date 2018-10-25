7523bdf29a13691d70bdcfd92df7abae69974e19
Totpoter <mindroixweetosser@gmail.com>
2018-10-25 09:27:26 +0200

    skapade inloggning och utloggning

--- index.php
+++ index.php
@@ -8,6 +8,24 @@
     <title>Login / Register</title>
   </head>
   <body>
+    <?php
+    if(isset($_SESSION['login'])) {
+      if($_SESSION['login'] == true) {
+        echo "Välkommen " . $_SESSION['login_name'];
+        ?>
+    <p>INLOGGAD</p><br>
+      <form action="logout.php">
+      <input type="submit" value="Logga ut">
+      </form>
+
+      <?php
+    }
+     else {
+      if(isset($_SESSION['error'])){
+      echo $_SESSION['error']. "<br>";
+      unset($_SESSION['error']);
+    }
+    ?>
     <div class="container">
       <div class="row">
         <div class="col-md-6">
@@ -19,13 +37,23 @@
       </form>
     </div>
     <div class="col-md-6">
-      <form class="forms" action="login.php" method="POST">
+      <form class="forms" action="submit.php" method="POST">
         <label for="username">Username:</label> <input type="username" name="login_name"><br>
         <label for="password">Password:</label> <input type="password" name="login_password"><br>
         <input class="submit" type="submit" value="Login">
       </form>
     </div>
   </div>
+  <?php
+    }
+  }
+
+      else {
+        $_SESSION['login'] = false;
+        header('Location: index.php');
+      }
+
+      ?>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </body>
 </html>
--- submit.php
+++ submit.php
@@ -12,5 +12,38 @@ if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password
 
   header("Location: index.php");
 
+  if(isset($_SESSION['login'])){
+      if($_SESSION['login'] == true){
+          header("location: index.php");
+      }
+  }
+  else{
+      $_SESSION['login'] = false;
+  }
 }
+  if(isset($_POST['login_name']) && isset($_POST['login_password'])){
+      $dbc_form = mysqli_connect("localhost","root","","v43_db");
+
+      $uid = $_POST['login_name'];
+      $pass = $_POST['login_password'];
+
+      $query = "SELECT * FROM users WHERE username='$uid' AND password = '$pass'";
+
+      $result = mysqli_query($dbc_form,$query);
+
+      if(mysqli_num_rows($result) == 1){
+          $row = mysqli_fetch_array($result);
+
+          $_SESSION['login'] = true;
+          $_SESSION['login_name'] = $row['username'];
+          header("location: index.php");
+      }
+      else{
+          $_SESSION['login'] = false;
+          $_SESSION['error'] = "FEL UPPGIFTER!";
+          header("location: index.php");
+      }
+
+  }
+
 ?>
